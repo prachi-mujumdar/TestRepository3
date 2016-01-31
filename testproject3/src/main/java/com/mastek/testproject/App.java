e7a6dfb6017976aa17d8e4015d1121114b06606b
prachi-mujumdar <prachi.mujumdar@mastek.com>
2016-02-01 03:17:15 +0530

    US9: implements task #1 with status: In-Progress; to do: 1 hours, estimates: 4 hours (updated)

---
+++ testproject3/src/main/java/com/mastek/testproject/App.java
@@ -0,0 +1,15 @@
+package com.mastek.testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		 System.out.println( "Welcome" );
+		 System.out.println( "Welcome Again" );
+    }
+}
